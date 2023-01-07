0a91b569ad0f5ca10805277aaa4d779fb6ee05a5
hefeng6500 <2443992009@qq.com>
2023-01-07 21:06:43 +0800

    添加博客评论和访问量

--- docs/components/UtterancCommment/index.vue
+++ docs/components/UtterancCommment/index.vue
@@ -4,13 +4,14 @@
 
 <script setup>
 import { onMounted } from "vue";
+
 onMounted(() => {
   const utterances = document.createElement("script");
   utterances.type = "text/javascript";
   utterances.async = true;
-  utterances.setAttribute("issue-term", "title");
+  utterances.setAttribute("issue-term", "pathname");
   utterances.setAttribute("theme", "github-light");
-  utterances.setAttribute("repo", "hefeng6500/interview-blog");
+  utterances.setAttribute("repo", "hefeng6500/java-blog");
   utterances.crossorigin = "anonymous";
   utterances.src = "https://utteranc.es/client.js";
   document.getElementById("content").appendChild(utterances);
